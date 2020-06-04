cdfb4ed1e762887e9311d5f67596200816736332
Hugo <hugo.beaupel@epitech.eu>
2020-06-04 17:26:54 +0200

    start implementing smart plug api get request

--- smartplug_http_common.py
+++ smartplug_http_common.py
@@ -1,9 +1,21 @@
 import requests
 import json
 
-response = requests.get("http://192.168.33.1/status")
-# Print the status code of the response.
-#print(response.content)
-data = json.loads(response.text)
+properties = ""
 
-print(json.dumps(json.dumps(data, indent=4, sort_keys=True)))
+
+def get_request(command="status"):
+    global properties
+    ip = ""
+    for p in properties:
+        p = p.split("=")
+        if p[0] == "ip":
+            ip = p[1]
+    response = requests.get("http://" + ip + '/' + command)
+    data = json.loads(response.text)
+    print(json.dumps(json.dumps(data, indent=4, sort_keys=True)))
+
+
+def smartplug_setup(filename):
+    global properties
+    properties = open(filename, "r")
