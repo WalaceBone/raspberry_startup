1167c731439c92568426ee3188d2b80af68984b6
Hugo <hugo.beaupel@epitech.eu>
2020-06-05 10:14:34 +0200

    post request for shelly api try stuff with json for parameters

--- smartplug_http_common.py
+++ smartplug_http_common.py
@@ -1,8 +1,10 @@
 import requests
 import json
 
-properties = ""
+from os import listdir
 
+properties = ""
+smartplug_api_params = "./smartplug/"
 
 def get_request(command="status"):
     global properties
@@ -14,8 +16,36 @@ def get_request(command="status"):
     response = requests.get("http://" + ip + '/' + command)
     data = json.loads(response.text)
     print(json.dumps(json.dumps(data, indent=4, sort_keys=True)))
+    return data
+
+
+def get_request_parameters(command):
+    for file in listdir(smartplug_api_params):
+        if file.find(command, 0, len(command)) == 0:
+            with open(smartplug_api_params+file, 'r') as json_params:
+                data = json.load(json_params)
+    return data
+
+
+def post_request(command):
+    global properties
+    ip = ""
+    for p in properties:
+        p = p.split("=")
+        if p[0] == "ip":
+            ip = p[1]
+
+    data = get_request_parameters(command)
+
+    response = requests.post("http://" + ip + '/' + command, data)
+
+    return
 
 
 def smartplug_setup(filename):
     global properties
     properties = open(filename, "r")
+
+
+if __name__ == '__main__':
+    print(get_request_parameters('sta'))
